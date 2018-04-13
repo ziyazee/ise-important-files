3d1e98aa6d367a2bc5982b831eb703608784ce64
ziyazee <ziyamohd123@gmail.com>
2018-04-13 20:34:09 +0530

    added forms to post a message

---
+++ firstproject/letsdoit/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+
+
+from .models import filest
+
+class PostForm(forms.ModelForm):
+    # def get_absolute_url():
+    #     return "/files/%s"
+    # categorie=get_absolute_url()
+    # def details(request,name):
+    #     category=filest.objects.get(categorie=name)    
+    class Meta:
+        # category=clasyear.objects.get(categorie=name)
+
+        #here i need to assign categorie value from url parameter
+        #categorie=**something**
+        
+        model = filest
+        fields = ('fname','fdescription','categorie',)
+        # widgets = {'categorie': forms.HiddenInput()}
+        
--- firstproject/letsdoit/views.py
+++ firstproject/letsdoit/views.py
@@ -1,24 +1,35 @@
 from django.shortcuts import render
 from django.http import Http404
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from .models import clasyear
 from .models import filest
+from .forms import PostForm
 def home(request):
     yea=clasyear.objects.order_by('year').values('year').distinct()
     return render(request,'home.html',{'yea':yea,})
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("great!!! files got added")
 # def getlist(request,id):
 #     try:
 def details(request,name):
     detail=clasyear.objects.filter(name=name)
-    return render(request,'details.html',{'detail':detail})
+    # category=filest.objects.get(categorie=name)
+    
+    contentValue = {'detail':detail}
+    return render(request,'details.html',contentValue)
 def subjects(request,year):
     subject=clasyear.objects.filter(year=year)
     return render(request,'subjects.html',{'subject':subject})
 def files(request,categorie):
     file=filest.objects.filter(categorie=categorie)    
-    return render(request,'files.html',{'file':file})
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/index/')
+    else:
+        form = PostForm()
+    return render(request,'files.html',{'file':file,'form':form})
 
 
   
